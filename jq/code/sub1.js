329311ae31745b55ab1f81366f6fa1b7064456b9
kosi6971 <84139325+kosi6971@users.noreply.github.com>
2022-11-15 13:14:50 +0900

    2022/11/15/1

--- jq/code/sub1.js
+++ jq/code/sub1.js
@@ -4,13 +4,15 @@ $(()=>{
     // 시작이벤트 글자
 
     // 스크롤/////////
-    // 카드집 파괴
 
+    const card = $(".card_house");
+
+
+    // 카드집 파괴
     // 카드파괴 - 실제 스크롤 높이
     const cardfull = $(".mntxt1").offset().top;
     // 카드파괴 - 원하는 공간의 높이
-    const cardH = $(window).hight();
-
+    const cardH = $(window).height();
     // 스크롤 위치 이동값
     let sctop;
     
@@ -19,13 +21,17 @@ $(()=>{
     $(window).scroll(function(){
         sctop = $(this).scrollTop();
 
+        console.log(cardH);
         console.log(sctop);
+        console.log(cardfull);
 
-        let cardposition = cardH*sctop/cardfull;
+        let cardposition = cardH*(sctop-cardH*1.935)/cardfull;
         // 스크롤 이동 가능 길이 : 보이는 화면 길이 = 스크롤 위치 이동값 : 이미지 이동값
         // 이미지 이동값 = 보이는 화면 길이 * 스크롤 위치 이동값 / 스크롤 이동 가능 길이
 
-
+        // console.log(cardposition);
+        if(sctop >= 0 && sctop <= cardH)
+        card.css({top:-cardposition+"px"});
 
     });
 });
